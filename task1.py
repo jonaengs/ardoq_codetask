0ac9e047de64c094861b8c366f065b57f7170ac3
Jonatan Engstad <jonaengs@gmail.com>
2020-10-20 09:34:57 +0200

    improve task 1

--- task1.py
+++ task1.py
@@ -1,24 +1,18 @@
 from math import prod as product
 from numbers import Number
+from itertools import combinations
 
 """
 Finds the highest value product producible by three elements from the given list
 Interpreted highest as meaning most positive, so 2 is higher than 1, and -1 is higher than -2
 """
 def max_product(l):
-    assert len(l) >= 3
-    assert all(isinstance(n, Number) for n in l)
-
-    neg = sorted(filter(lambda x: x < 0, l)) # sorted negative values
-    pos = sorted(filter(lambda x: x > 0, l)) # sorted positive values
-
-    return max(
-        pos[-1] * product(neg[:2]) if len(pos) >= 1 and len(neg) >= 2 else float("-inf"), 
-        product(pos[-3:]) if len(pos) >= 3 else float("-inf"), 
-        product(neg[-3:]) if len(neg) >= 3 else float("-inf")
-    )
+    assert len(l) >= 3 and all(isinstance(n, Number) for n in l),\
+        "List must contain only numbers and have a length gte 3"
+    return max(map(product, combinations(l, 3)))
 
 
 assert max_product([1, 10, 2, 6, 5, 3] ) == 300  # highest value requires product of three positive numbers
 assert max_product([1, 2, -3, 4, 5, -7]) == 105  # highest value requires product of two negative numbers
 assert max_product([-1, -2, -3, -4, -5]) == -6  # highest value is smallest possible negative value
+assert max_product([1, 2, -3]) == -6
